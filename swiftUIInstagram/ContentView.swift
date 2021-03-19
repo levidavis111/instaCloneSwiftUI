50629d1fe950eb5a78ae3a05e5e97561376f095b
levi davis <ldd235@gmail.com>
2021-03-19 13:35:06 -0400

    Initial Commit

---
+++ swiftUIInstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIInstagram
+//
+//  Created by Levi Davis on 3/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIInstagram/swiftUIInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIInstagramApp.swift
+//  swiftUIInstagram
+//
+//  Created by Levi Davis on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
