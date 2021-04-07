b964edce769814055dc3ebdde4d03ec7362353e3
levi davis <ldd235@gmail.com>
2021-04-07 13:54:45 -0400

    Adds Upload image view

--- swiftUIInstagram/View/UploadPost/UploadPostView.swift
+++ swiftUIInstagram/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
     var body: some View {
-        Text("Upload Post")
+        VStack {
+            if postImage == nil {
+                Button(action: {}, label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                        
+                })
+            } else {
+                HStack(alignment: .top) {
+                    Image("dog")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption...", text: $captionText)
+                }
+                .padding()
+                Button(action: {}, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                })
+                .padding()
+            }
+            
+            Spacer()
+        }
     }
 }
 
