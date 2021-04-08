3d801a8d4d8685d843194357697e32e6afd5c5c0
levi davis <ldd235@gmail.com>
2021-04-08 10:59:31 -0400

    Adds navigation link to sign up button

--- swiftUIInstagram/View/Authentication/LoginView.swift
+++ swiftUIInstagram/View/Authentication/LoginView.swift
@@ -66,17 +66,21 @@ struct LoginView: View {
                     //go to signup button
                     Spacer()
                     
-                    Button(action: {}, label: {
-                        HStack {
-                            Text("Don't have an account?")
-                                .font(.system(size: 14))
-                            Text("Sign Up")
-                                .font(.system(size: 14, weight: .semibold))
-                        }
-                        .foregroundColor(.white)
-                    })
-                    .padding(.bottom, 32)
+                    NavigationLink(
+                        destination: RegistrationView(),
+                        label: {
+                            HStack {
+                                Text("Don't have an account?")
+                                    .font(.system(size: 14))
+                                Text("Sign Up")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .foregroundColor(.white)
+                        })
+                        .padding(.bottom, 16)
+                    
                 }
+                .padding(.top, -16)
             }
         }
     }
